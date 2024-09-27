86bfd0b1fcef2d04a6e702f1eb0a22b946a985c2
saisantosh2805 <saisantosh2000naviri@gmail.com>
2024-09-27 13:27:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const charCount={
+		for(let char of str){
+			charCount[char]=(charCount[char]||0)+1;
+		}
+		for (let char of str){
+		if(charcount[char]===1)
+			
+		}
+	{
+			return char;
+	}
+	else(char count[Char])
+	{
+		return null;
+	}
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+	}
+}
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
